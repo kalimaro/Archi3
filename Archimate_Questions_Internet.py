b50bde93c7e2427bf0bc6f1508f946a3fbf5539f
kalimaro <126461396+kalimaro@users.noreply.github.com>
2024-11-23 07:52:50 +0000

    Update Archimate_Questions_Internet.py

--- Archimate_Questions_Internet.py
+++ Archimate_Questions_Internet.py
@@ -36,7 +36,7 @@ for qt in newlist:
     csv_reader = csv.reader(f)
     for line in csv_reader:
         if line[0]== qt :
-            st.write(str(questions_nums)+") "+line[1])
+            st.write(str(questions_nums)+') '+line[1])
             st.write(line[2])
             st.write(line[3])
             st.write(line[4])
