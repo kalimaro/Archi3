e442f6339cb691ad9136b9ed9c39e918ed0cf05c
kalimaro <126461396+kalimaro@users.noreply.github.com>
2024-11-23 18:08:46 +0000

    Update Archimate_Questions_Internet.py

--- Archimate_Questions_Internet.py
+++ Archimate_Questions_Internet.py
@@ -79,4 +79,6 @@ final_score = rights/len(list_responses)*100
 st.write("Your final score is: "+str(final_score)+"%")
 for key in st.session_state.keys():
     del st.session_state[key]
+st.session_state.input_value = ""
+st.rerun()
 st.stop()
