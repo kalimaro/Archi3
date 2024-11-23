ec5f582f4aa59d8497faadd1fc41c00438024bb4
kalimaro <126461396+kalimaro@users.noreply.github.com>
2024-11-23 21:34:29 +0000

    Update Archimate_Questions_Internet.py

--- Archimate_Questions_Internet.py
+++ Archimate_Questions_Internet.py
@@ -14,19 +14,18 @@ right_questions = []
 if 'responses' not in st.session_state:
     st.session_state.responses = {}
 st.title("Archimate Mock Test!")
+questions_nums = 1
+f = open ('Archimate_Data_Bank.csv',"r", encoding='UTF8')
+csv_reader = csv.reader(f)
+for line in csv_reader:
+    questions_list.append(line[0])
+f.close()
+
+shuffle(questions_list)
+newlist = questions_list[:4]
+
+
 with st.form(key="my_form", clear_on_submit=True, enter_to_submit=False):
-    questions_nums = 1
-    f = open ('Archimate_Data_Bank.csv',"r", encoding='UTF8')
-    csv_reader = csv.reader(f)
-    for line in csv_reader:
-        questions_list.append(line[0])
-    f.close()
-
-    shuffle(questions_list)
-    newlist = questions_list[:4]
-
-
-#with st.form(key="my_form", clear_on_submit=True, enter_to_submit=False):
     for qt in newlist:
         f = open ('Archimate_Data_Bank.csv',"r", encoding='UTF8')
         csv_reader = csv.reader(f)
